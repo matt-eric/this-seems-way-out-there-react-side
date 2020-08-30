c14526a90ca0d91466ed5806f3e6e21bef78fb98
matt-eric <mf9646a@alumni.american.edu>
2020-08-30 02:44:14 -0400

    Add gridcell feature

--- src/App/src/Components/Simulator/Simulator.js
+++ src/App/src/Components/Simulator/Simulator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 import { PtsCanvas } from 'react-pts-canvas'
-import { Pt, Create } from 'pts/dist/es5'
+import { Pt, Create, Rectangle, Color } from 'pts/dist/es5'
 
 class ConfigureSimulation extends PtsCanvas {
 
@@ -9,6 +9,8 @@ class ConfigureSimulation extends PtsCanvas {
     super(props);
     this.noiseLine = []
     this.noiseGrid = []
+    this.follower = new Pt();
+    this.pts = []
   }
 
   start() {
@@ -17,6 +19,8 @@ class ConfigureSimulation extends PtsCanvas {
     let gd = Create.gridPts( this.space.innerBound, 20, 20 );
     this.noiseLine = Create.noisePts( ln, 0.1, 0.1 );
     this.noiseGrid = Create.noisePts( gd, 0.05, 0.1, 20, 20 );
+    this.pts = Create.gridCells( this.space.innerBound, 40, 20 );
+    this.follower = this.space.center;
   }
 
   animate( time, ftime ) {
@@ -38,7 +42,19 @@ class ConfigureSimulation extends PtsCanvas {
     // Draw wave
     nps = nps.concat( [this.space.size, new Pt( 0, this.space.size.y )] );
     this.form.fillOnly("rgba(0,140,255,.65)").polygon( nps );
-    this.form.fill("#fff").points( nps, 2, "circle");
+    this.form.fill("#fff").points( nps, 2, "square");
+
+    this.follower = this.follower.add( this.space.pointer.$subtract( this.follower ).divide(20) );
+
+    this.form.stroke("#123");
+
+    // calculate the size and color of each cell based on its distance to the pointer
+    let rects = this.pts.map( (p) => {
+      let mag = this.follower.$subtract( Rectangle.center( p ) ).magnitude()
+      let scale = Math.min( 1, Math.abs( 1 - ( 0.7 * mag / this.space.center.y ) ) );
+      let r = Rectangle.fromCenter( Rectangle.center(p), Rectangle.size(p).multiply( scale ) );
+      this.form.fill( Color.HSLtoRGB( Color.hsl( scale*375, 1, 3 ) ).hex ).rect( r );
+    })
   }
 }
 
@@ -47,7 +63,11 @@ export default class App extends Component {
     return (
 
       <>
-        <ConfigureSimulation background="#0c9" name="pts-tester" style={{opacity: 0.95}} />
+        <ConfigureSimulation
+          background="#0c9"
+          name="pts-tester"
+          style={{opacity: 0.95, height: '100vh'}}
+        />
       </>
     )
   }
