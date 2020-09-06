3b10c57497849745c3afa71c30bdf2c5dfed5e69
matt-eric <mf9646a@alumni.american.edu>
2020-09-05 23:18:13 -0400

    Add AppBar Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,111 @@
 import React from 'react';
-// import AuthRouter from '../../AuthRouter';
 import { BrowserRouter } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { loadCSS } from 'fg-loadcss';
 import { theme } from './theme'
+import clsx from 'clsx';
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton
+} from '@material-ui/core';
+import {
+  MenuIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon
+} from '@material-ui/icons';
 import Routing from './Routing'
 
 const MuiTheme = createMuiTheme(theme)
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: '#26ce9e'
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  headerFont: {
+    fontFamily: "'MuseoModerno', cursive",
+    color: '#26ce9e'
+  },
+}));
+
 function App() {
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  React.useEffect(() => {
+    const node = loadCSS(
+      'https://use.fontawesome.com/releases/v5.12.0/css/all.css',
+      document.querySelector('#font-awesome-css'),
+    );
+
+    return () => {
+      node.parentNode.removeChild(node);
+    };
+  }, []);
 
   return (
 
@@ -18,7 +115,47 @@ function App() {
 
         <MuiThemeProvider theme={MuiTheme}>
 
-          <Routing/>
+            <AppBar position="fixed" className={clsx(classes.appBar, { [classes.appBarShift]: open, })}>
+
+              <Toolbar>
+
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  className={clsx(classes.menuButton, open && classes.hide)}
+                >
+                  <MenuIcon/>
+                </IconButton>
+
+                <Typography variant='h6' className={classes.headerFont}>
+                  WAVEFORMS.APP
+                </Typography>
+
+              </Toolbar>
+
+            </AppBar>
+
+            <Drawer
+              className={classes.drawer}
+              variant="persistent"
+              anchor="left"
+              open={open}
+              classes={{ paper: classes.drawerPaper }}
+            >
+
+              <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawerClose}>
+                  {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+              </div>
+
+            </Drawer>
+
+            <main className={clsx(classes.content, { [classes.contentShift]: open, })}>
+              <Routing/>
+            </main>
 
         </MuiThemeProvider>
 
