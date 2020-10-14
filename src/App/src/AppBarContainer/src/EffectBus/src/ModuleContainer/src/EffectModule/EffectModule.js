7357926bf8f67f110d84dea69d023ce85d5f433c
matt-eric <mf9646a@alumni.american.edu>
2020-10-13 22:04:36 -0400

    Restructure toolbar icon json

--- src/App/src/AppBarContainer/src/EffectBus/src/ModuleContainer/src/EffectModule/EffectModule.js
+++ src/App/src/AppBarContainer/src/EffectBus/src/ModuleContainer/src/EffectModule/EffectModule.js
@@ -1,16 +1,35 @@
 import React, { useEffect } from 'react';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux'
 import allActions from '../../../../../../../../../redux/actions'
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import WidgetsIcon from '@material-ui/icons/Widgets';
+import {
+  IconButton,
+  Paper
+ } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   module: {
-    width: '200px',
-    height: '200px',
+    width: '300px',
+    height: '300px',
     margin: '5px',
+    padding: '5px',
     backgroundColor: '#04080a',
-    border: '2px solid #69f0ae'
+    border: '2px solid #69f0ae',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  toolBar: {
+    display: 'flex',
+    flexDirecton: 'row',
+    justifyContent: 'space-between'
+  },
+  interface: {
+    display: 'flex',
+    flexDirecton: 'column'
   }
 }));
 
@@ -20,11 +39,30 @@ export default function EffectModule(props){
 
   const classes = useStyles();
 
+  const toolBarIcons = [
+    {
+      icon: <PowerSettingsNewIcon />,
+      tooltip: 'Bypass'
+    },
+    {
+      icon: <WidgetsIcon />,
+      tooltip: 'Module Type'
+    },
+    {
+      icon: <SaveIcon />,
+      tooltip: 'Save As Preset'
+    },
+    {
+      icon: <DeleteForeverIcon />,
+      tooltip: 'Remove Module'
+    },
+  ]
+
   const {
     effectModules
   } = useSelector(state => state.effectBus);
 
-  const setEffectBusData = (type, data) =>     dispatch(allActions.effectBusActions.setEffectBusData(type, data))
+  const setEffectBusData = (type, data) => dispatch(allActions.effectBusActions.setEffectBusData(type, data))
 
   return (
     <Paper
@@ -33,7 +71,18 @@ export default function EffectModule(props){
       {...props.draggableProps}
       {...props.dragHandleProps}
     >
-      hello world
+      <div className={classes.toolBar}>
+        {toolBarIcons.map(obj =>
+          <IconButton color="secondary" >
+            {obj.icon}
+          </IconButton>
+        )}
+      </div>
+      <div className={classes.interface}>
+
+        {/*interface content goes here*/}
+
+      </div>
     </Paper>
   );
 }
