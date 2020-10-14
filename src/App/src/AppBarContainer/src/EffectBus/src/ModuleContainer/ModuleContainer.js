c99c414911055dc63cd26582ac5a2163022dedfa
matt-eric <mf9646a@alumni.american.edu>
2020-10-13 21:35:34 -0400

    Implement 'add module' button

--- src/App/src/AppBarContainer/src/EffectBus/src/ModuleContainer/ModuleContainer.js
+++ src/App/src/AppBarContainer/src/EffectBus/src/ModuleContainer/ModuleContainer.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import EffectModule from './src/EffectModule'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux'
 import allActions from '../../../../../../../redux/actions'
@@ -11,6 +13,17 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'row'
   },
+  addButton: {
+    height: '300px',
+    color: '#69f0ae',
+    backgroundColor: '#04080a',
+    border: '2px solid #69f0ae',
+    margin: '12.5px',
+    '&:hover': {
+        backgroundColor: '#04080a',
+        color: '#69f0ae'
+    }
+  }
 }));
 
 export default function ModuleContainer(){
@@ -23,7 +36,7 @@ export default function ModuleContainer(){
     effectModules
   } = useSelector(state => state.effectBus);
 
-  const setEffectBusData = (type, data) =>     dispatch(allActions.effectBusActions.setEffectBusData(type, data))
+  const setEffectBusData = (type, data) => dispatch(allActions.effectBusActions.setEffectBusData(type, data))
 
   const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
@@ -52,7 +65,7 @@ export default function ModuleContainer(){
           id: `item-${k}`,
           content: `item ${k}`,
         }));
-      dispatch(allActions.effectBusActions.setEffectBusData('effectModules', mapModules(14)))
+      dispatch(allActions.effectBusActions.setEffectBusData('effectModules', mapModules(8)))
     }
   }, [dispatch, effectModules])
 
@@ -60,22 +73,29 @@ export default function ModuleContainer(){
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="droppable" direction="horizontal">
         {(droppableProvided, droppableSnapshot) => (
-          <div
-            ref={droppableProvided.innerRef}
-            className={classes.effectModuleContainer}
-          >
-            {effectModules && effectModules.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(draggableProvided, draggableSnapshot) => (
-                  <EffectModule
-                    innerRef={draggableProvided.innerRef}
-                    draggableProps={draggableProvided.draggableProps}
-                    dragHandleProps={draggableProvided.dragHandleProps}
-                  />
-                )}
-              </Draggable>
-            ))}
-            {droppableProvided.placeholder}
+          <div className={classes.effectModuleContainer}>
+            <div>
+              <Button className={classes.addButton} variant='contained'>
+                <AddIcon fontSize='large'/>
+              </Button>
+            </div>
+            <div
+              ref={droppableProvided.innerRef}
+              className={classes.effectModuleContainer}
+            >
+              {effectModules && effectModules.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(draggableProvided, draggableSnapshot) => (
+                    <EffectModule
+                      innerRef={draggableProvided.innerRef}
+                      draggableProps={draggableProvided.draggableProps}
+                      dragHandleProps={draggableProvided.dragHandleProps}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {droppableProvided.placeholder}
+            </div>
           </div>
         )}
       </Droppable>
