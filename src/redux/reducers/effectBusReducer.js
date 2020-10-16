5b4a32d96942386b3d50784caf267971161dcdb1
matt-eric <mf9646a@alumni.american.edu>
2020-10-15 21:36:57 -0400

    Add displayName parameter to effect objects

--- src/redux/reducers/effectBusReducer.js
+++ src/redux/reducers/effectBusReducer.js
@@ -4,14 +4,17 @@ const effectBus = (state = {
   existingEffects: [
     {
       type: 'noiseGrid',
+      displayName: 'Noise Grid',
       params: {}
     },
     {
       type: 'waveform',
+      displayName: 'Waveform',
       params: {}
     },
     {
       type: 'cellBlocks',
+      displayName: 'Cell Blocks',
       params: {
         sparkleFocus: .87,
       }
