96e11c60946843f89f84f09345e3d3221685f92d
matt-eric <mf9646a@alumni.american.edu>
2020-11-04 22:44:03 -0500

    Fix #72 Comment out dev tools conditionality

--- src/configureStore.js
+++ src/configureStore.js
@@ -13,9 +13,9 @@ export default function configureStore(initialState = {}, history) {
 
   // If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
   /* istanbul ignore next */
-  if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
+  // if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
     /* eslint-disable no-underscore-dangle */
-    if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+    // if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
       composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
 
     // NOTE: Uncomment the code below to restore support for Redux Saga
@@ -25,7 +25,7 @@ export default function configureStore(initialState = {}, history) {
     //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
     //   };
     /* eslint-enable */
-  }
+  // }
 
   const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
 
