b0cd212971a69ce2861050c73174290f17e6a47c
matt-eric <mf9646a@alumni.american.edu>
2020-11-04 21:26:12 -0500

    Fix #58 Add modular GitHub link component

---
+++ src/App/src/AppBarContainer/src/GitHubModule/GitHubModule.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  IconButton,
+  Paper,
+} from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#0A0A0A',
+    marginRight: '10px',
+    border: '1px solid #26ce9e',
+    paddingRight: '10px'
+  },
+  icon: {
+    color: '#26ce9e',
+  },
+}));
+
+function GitHubModule(props) {
+
+  const classes = useStyles();
+
+  const openGitHub = (repo) => {
+    window.open(`https://github.com/matt-eric/${repo}`)
+  }
+
+  return (
+
+    <Paper className={classes.paper} onClick={() => openGitHub(props.endpoint)} target="_blank">
+      <IconButton className={classes.icon} >
+        <GitHubIcon/>
+      </IconButton>
+      <img
+        draggable="false"
+        src={props.svg}
+        width={"100%"}
+        alt={props.alt}
+      />
+    </Paper>
+
+  );
+
+}
+
+export default GitHubModule;
