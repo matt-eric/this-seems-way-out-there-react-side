504cea01b76d65d370cc0a8ae0e2f8d23f1e5dbd
matt-eric <mf9646a@alumni.american.edu>
2020-11-07 11:43:08 -0500

    Remove Okta sign-in methodology from Authenticator component

--- src/App/src/AppBarContainer/src/EffectBus/src/OptionsInterface/src/AuthenticationDialog/src/Authenticator/Authenticator.js
+++ src/App/src/AppBarContainer/src/EffectBus/src/OptionsInterface/src/AuthenticationDialog/src/Authenticator/Authenticator.js
@@ -1,59 +1,12 @@
-import React from 'react';
-import {
-  Route,
-  Switch,
-  withRouter
-} from 'react-router-dom'
-import { Security, SecureRoute, LoginCallback } from '@okta/okta-react'
-import Login from './src/Login'
+import React from "react";
 
-const loading = () => <div/>
+const Authenticator = () => {
 
-export default withRouter(class AuthRouter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onAuthRequired = this.onAuthRequired.bind(this);
-  }
+  return (
+    <div>
+      Authenticator Component
+    </div>
+  );
+};
 
-  onAuthRequired() {
-    this.props.history.push('/login')
-  }
-
-  render() {
-
-    return (
-
-        <Security
-          issuer={process.env.REACT_APP_OKTA_ISSUER}
-          clientId={process.env.REACT_APP_OKTA_CLIENT_ID}
-          redirectUri={window.location.origin + '/implicit/callback'}
-          onAuthRequired={this.onAuthRequired}
-          pkce={true}
-        >
-
-          <React.Suspense fallback={loading()} >
-
-            <Switch>
-
-              <Route path='/implicit/callback' component={LoginCallback}/>
-
-              <Route
-                exact
-                path='/'
-                name='Login'
-                render={() => <Login baseUrl={process.env.REACT_APP_OKTA_BASE_URL}/>}
-              />
-
-              <SecureRoute exact path='/' name='Navigator' />
-
-            </Switch>
-
-          </React.Suspense>
-
-        </Security>
-
-    );
-
-  }
-
-});
+export default Authenticator;
